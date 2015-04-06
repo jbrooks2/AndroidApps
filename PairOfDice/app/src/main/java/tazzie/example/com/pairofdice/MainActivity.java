00b5bab5f8f5a13b2d1dae4749f59ee2997eeef2
hal255 <hluu114@gmail.com>
2015-04-05 23:02:44 -0700

    Pair Of Dice
    CS 454 - 1
    Spring 2015

--- PairOfDice/app/src/main/java/tazzie/example/com/pairofdice/MainActivity.java
+++ PairOfDice/app/src/main/java/tazzie/example/com/pairofdice/MainActivity.java
@@ -1,17 +1,66 @@
 package tazzie.example.com.pairofdice;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+    private FrameLayout die1, die2;
+    private Button roll_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        die1 = (FrameLayout) findViewById(R.id.die1);
+        die2 = (FrameLayout) findViewById(R.id.die2);
+        roll_button = (Button) findViewById(R.id.roll_button);
+        roll_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+    }
+
+    // generates 2 random integer between 1 and 6 inclusive
+    public void rollDice(){
+        int val1 = 1 + (int)(6 * Math.random());
+        int val2 = 1 + (int)(6 * Math.random());
+        setDie(val1, die1);
+        setDie(val2, die2);
+    }
+
+    // set appropriate picture to die
+    public void setDie(int value, FrameLayout layout){
+        Drawable pic = null;
+        switch (value){
+            case 1:
+                pic = getResources().getDrawable(R.drawable.die_face_1);
+                break;
+            case 2:
+                pic = getResources().getDrawable(R.drawable.die_face_2);
+                break;
+            case 3:
+                pic = getResources().getDrawable(R.drawable.die_face_3);
+                break;
+            case 4:
+                pic = getResources().getDrawable(R.drawable.die_face_4);
+                break;
+            case 5:
+                pic = getResources().getDrawable(R.drawable.die_face_5);
+             break;
+            case 6:
+                pic = getResources().getDrawable(R.drawable.die_face_6);
+                break;
+        }
     }
 
     @Override
